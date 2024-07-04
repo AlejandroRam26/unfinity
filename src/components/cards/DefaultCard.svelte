a010c28a74439d999065ae04dbdf193c16042b1f
AlejandroRam26 <aleramirez0326@gmail.com>
2024-07-04 16:00:58 -0300

    Adding pre-Cards

---
+++ src/components/cards/DefaultCard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let num = 0;
+</script>
+
+<div
+  class="w-[100px] h-[200px] flex items-center justify-center bg-white rounded-lg"
+>
+  <div class="w-full h-full bg-crimson"></div>
+  <h2 class="text-2xl">{num}</h2>
+</div>
